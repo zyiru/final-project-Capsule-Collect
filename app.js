db86aca722d45a2ab4023aee1f5c9735a002d8ed
Yiruz <2yi.z.ru2@gmail.com>
2020-04-07 21:26:23 -0400

    connect to mongdb and click button to increment quantity value of object in database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('./db');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -24,8 +25,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+const Piece = mongoose.model('Piece');
+
 app.get('/', (req, res) => {
-  res.render('home');
+	res.render('home',{piece:randPiece});
+});
+
+let randPiece={};
+app.post('/', (req, res)=>{
+	Piece.aggregate([{$sample:{size:1}}], (err, result)=>{
+		Piece.update({name: result[0].name}, {$inc: {quantity: 1}});
+	});
+	randPiece = {p: result[0], msg: "You Got "};
+	res.redirect('/');
+});
+
+app.get('/collection/pieces', (req,res) => {
+	Piece.find({}, (err, result)=>{
+		res.render('collection', {collection: result});
+	})
 });
 
 app.listen(3000);
--- db.js
+++ db.js
@@ -1,21 +1,15 @@
 //1ST DRAFT
 const mongoose = require('mongoose');
 
-const User = new mongoose.Schema({
-  pieces: [Piece],
-  toys: [Toy],
-  pets: Pet
-});
-
 const Piece = new mongoose.Schema({
   name: {type: String, required: true},
-  quantity: {type: Number, min: 0, required: true},
+  quantity: {type: Number, required: true},
 });
 
 
 const Toy = new mongoose.Schema({
   name: {type: String, required: true},
-  quantity: {type: Number, min: 0, required: true},
+  quantity: {type: Number, required: true},
 });
 
 const Pet = new mongoose.Schema({
@@ -23,3 +17,32 @@ const Pet = new mongoose.Schema({
   level: {type: Number, min: 1, max: 5, required: true},
   growth: {type: Number, required: true},
 });
+
+const User = new mongoose.Schema({
+  pieces: [Piece],
+  toys: [Toy],
+  pets: Pet
+});
+
+mongoose.model('User', User);
+mongoose.model('Piece', Piece);
+
+/***** from hw06 *****/
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/finalproject';
+}
+mongoose.connect(dbconf,{useNewUrlParser: true, useUnifiedTopology: true});
